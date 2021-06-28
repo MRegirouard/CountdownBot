5b0cbcc44c3214505e390244b1e7db3d63864b4c
Ethan Girouard <mr.egirouard@gmail.com>
2021-06-28 14:19:30 -0400

    Implement ConfigReader.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const discord = require('discord.js')
 const configReader = require('./ConfigReader.js')
+
+const dClient = new discord.Client()
+
+const configFile = './CountDownBot.conf' // Path to config file
+const configOptions = { // Options to retrieve from config file
+    'Discord API Token':'',
+}
+
+configReader.readOptions(configFile, configOptions, false).then((result) =>
+{
+    console.info('Successfully read config information.')
+
+    dClient.login(result['Discord API Token'])
+})
+.catch((err) =>
+{
+    console.error(err)
+    process.exit(1)
+})
